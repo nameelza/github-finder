3bc65431bf43065b74f866e61c7c93d84b36ac91
nameelza <82921161+nameelza@users.noreply.github.com>
2022-05-13 14:24:39 -0700

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,21 @@
-function Navbar() {
+import { FaGithub } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+
+function Navbar({ title }) {
   return (
     <div>
       <h1>Github Finder</h1>
     </div>
   );
 }
+
+Navbar.defaultProps = {
+  title: "Github Finder",
+};
+
+Navbar.propTypes = {
+  title: PropTypes.string,
+};
 
 export default Navbar;
