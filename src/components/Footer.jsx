151abd5d1dc7924a89add6d699503e07886fefe2
nameelza <82921161+nameelza@users.noreply.github.com>
2022-05-16 16:12:08 -0700

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,9 @@
 function Footer() {
+  const footerYear = new Date().getFullYear();
+
   return (
-    <div>
-      <h1>Footer</h1>
+    <div className="footer p-10 bg-gray-700 text-primary-content footer-center">
+      <p>Copyright &copy; {footerYear} All rights reserved</p>
     </div>
   );
 }
