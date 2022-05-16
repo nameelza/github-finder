fa781c1d1081496bff00d1e24973e992c285c375
nameelza <82921161+nameelza@users.noreply.github.com>
2022-05-16 15:56:15 -0700

    footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="flex flex-col justify-between h-screen">
       <Navbar />
       <main>Content</main>
+      <Footer/>
     </div>
   );
 }
