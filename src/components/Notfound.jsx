77c495902330870dc6ea6882180aabbd5bccf1f7
nameelza <82921161+nameelza@users.noreply.github.com>
2022-05-17 13:49:45 -0700

    Update Notfound.jsx

--- src/components/Notfound.jsx
+++ src/components/Notfound.jsx
@@ -1,7 +1,11 @@
+import { Link } from "react-router-dom";
+
 function Notfound() {
   return (
-    <div>
-      <h1>Notfound</h1>
+    <div className="hero">
+      <Link to="/" className="btn btn-primary btn-lg">
+        Back To Home
+      </Link>
     </div>
   );
 }
