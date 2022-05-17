ec0b3c24521f88cef648c06b51cec32d657f5b54
nameelza <82921161+nameelza@users.noreply.github.com>
2022-05-17 13:53:42 -0700

    Update Notfound.jsx

--- src/components/Notfound.jsx
+++ src/components/Notfound.jsx
@@ -1,11 +1,23 @@
+import { FaHome } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 function Notfound() {
   return (
     <div className="hero">
-      <Link to="/" className="btn btn-primary btn-lg">
-        Back To Home
-      </Link>
+      <div className="hero-content text-center">
+        <div className="max-w-lg">
+          <h1 className="text-primary-content text-8xl text-bold mb-8">
+            Oops!
+          </h1>
+          <p className="text-primary-content text-5xl mb-8">
+            404 - Page not found
+          </p>
+          <Link to="/" className="btn btn-primary btn-lg">
+            <FaHome className="mr-2"/>
+            Back To Home
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
