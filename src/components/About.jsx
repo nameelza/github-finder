89d97b69fbe4e99251710110a4c512b3d4433736
nameelza <82921161+nameelza@users.noreply.github.com>
2022-05-17 13:46:02 -0700

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,8 +2,11 @@ function About() {
   return (
     <div>
       <h1 className="text-primary-content text-6xl mb-4">Github Finder</h1>
-      <p className="text-2xl mb-4 font-light">
+      <p className="text-primary-content text-2xl mb-4 font-light">
         A React app to search Github profiles and see profile details.
+      </p>
+      <p className="text-lg text-gray-400">
+        Version <span className="text-white">1.0.0</span>
       </p>
     </div>
   );
