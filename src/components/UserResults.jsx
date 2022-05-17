761bf8d18a4c91799e1b4dd56467e5b3c6bb5560
nameelza <82921161+nameelza@users.noreply.github.com>
2022-05-17 15:28:22 -0700

    token

--- src/components/UserResults.jsx
+++ src/components/UserResults.jsx
@@ -4,8 +4,13 @@ function UserResults() {
   useEffect(() => {
     fetchUsers();
   }, []);
+
   const fetchUsers = async () => {
-    const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`);
+    const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`, {
+      headers: {
+        Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
+      },
+    });
     const data = await response.json();
     console.log(data);
   };
