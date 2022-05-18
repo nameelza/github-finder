14df2fb10e0981896543e07e510deb2e03a4abe6
nameelza <82921161+nameelza@users.noreply.github.com>
2022-05-18 15:09:23 -0700

    Update GithubReducer.js

--- src/context/github/GithubReducer.js
+++ src/context/github/GithubReducer.js
@@ -1,5 +1,11 @@
 const githubReducer = (state, action) => {
   switch (action.type) {
+    case "GET_USERS":
+      return {
+        ...state,
+        users: action.data,
+        loading: false,
+      };
     default:
       return state;
   }
