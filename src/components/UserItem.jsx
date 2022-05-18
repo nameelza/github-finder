bdc655d75d7c2650200a0349227912cb831154e4
nameelza <82921161+nameelza@users.noreply.github.com>
2022-05-17 17:34:27 -0700

    css

--- src/components/UserItem.jsx
+++ src/components/UserItem.jsx
@@ -1,15 +1,17 @@
 import PropTypes from "prop-types";
 
-function UserItem(user) {
+function UserItem({ user: { login, avatar_url } }) {
   return (
-    <div>
-      <h3>{user.login}</h3>
+    <div className="card shadow-md compact side bg-base-100">
+      <div className="flex-row items-center space-x-4 card-body">
+        <h3>{login}</h3>
+      </div>
     </div>
   );
 }
 
 UserItem.propTypes = {
   user: PropTypes.object.isRequired,
-}
+};
 
 export default UserItem;
