0f0ffb472dfcb1e4f8582d7e597a7dd166bd50f6
nameelza <82921161+nameelza@users.noreply.github.com>
2022-05-17 17:36:52 -0700

    Update UserItem.jsx

--- src/components/UserItem.jsx
+++ src/components/UserItem.jsx
@@ -4,7 +4,14 @@ function UserItem({ user: { login, avatar_url } }) {
   return (
     <div className="card shadow-md compact side bg-base-100">
       <div className="flex-row items-center space-x-4 card-body">
-        <h3>{login}</h3>
+        <div>
+          <div className="avatar">
+            <div className="rounded-full shadow w-14 h-14">
+              <img src={avatar_url} alt="{login}" />
+            </div>
+          </div>
+        </div>
+        <div></div>
       </div>
     </div>
   );
