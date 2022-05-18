9268e3f8c49028c819f24a9e060b758252dbc918
nameelza <82921161+nameelza@users.noreply.github.com>
2022-05-18 13:06:20 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,21 +4,24 @@ import Footer from "./components/Footer";
 import Home from "./components/Home";
 import About from "./components/About";
 import Notfound from "./components/Notfound";
+import { GithubProvider } from "./context/github/GithubContext";
 
 function App() {
   return (
-    <div className="flex flex-col justify-between h-screen">
-      <Navbar />
-      <main className="container mx-auto px-3 pb-12">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/notfound" element={<Notfound />} />
-          <Route path="/*" element={<Notfound />} />
-        </Routes>
-      </main>
-      <Footer />
-    </div>
+    <GithubProvider>
+      <div className="flex flex-col justify-between h-screen">
+        <Navbar />
+        <main className="container mx-auto px-3 pb-12">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/notfound" element={<Notfound />} />
+            <Route path="/*" element={<Notfound />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </GithubProvider>
   );
 }
 
