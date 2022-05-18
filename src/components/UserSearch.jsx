6e9fd2e0b7bf81c8041a18e2c66f99dd473ddf94
nameelza <82921161+nameelza@users.noreply.github.com>
2022-05-18 15:51:46 -0700

    Update UserSearch.jsx

--- src/components/UserSearch.jsx
+++ src/components/UserSearch.jsx
@@ -2,10 +2,20 @@ function UserSearch() {
   return (
     <div className="grid grid-cols-1 xl:grid-xols-2 lg:grid-cols-2 md:grid-cols-2 mb-8 gap-8">
       <div>
-        <form></form>
+        <form>
+          <div className="form-control">
+            <div className="relative">
+              <input
+                type="text"
+                className="w-full pr-40 bg-gray-200 input input-lg text-black"
+                placeholder="Search"
+              />
+            </div>
+          </div>
+        </form>
       </div>
       <div>
-        <button className="btn btn-ghost btn-lg">Button</button>
+        <button className="btn btn-ghost btn-lg">Clear</button>
       </div>
     </div>
   );
