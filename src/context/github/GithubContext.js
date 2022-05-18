e9d7d5de1de7197f7d8bc937eb392ce745b05ffd
nameelza <82921161+nameelza@users.noreply.github.com>
2022-05-18 15:08:32 -0700

    Update GithubContext.js

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -1,4 +1,5 @@
-import { createContext, useState } from "react";
+import { createContext, useReducer } from "react";
+import githubReducer from "./GithubReducer";
 
 const GithubContext = createContext();
 
@@ -6,8 +7,10 @@ const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
 const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
 
 export const GithubProvider = ({ children }) => {
-  const [users, setUsers] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const [state, dispatch] = useReducer(githubReducer, {
+    users: [],
+    loading: true,
+  });
 
   const fetchUsers = async () => {
     const response = await fetch(`${GITHUB_URL}/users`, {
@@ -18,12 +21,20 @@ export const GithubProvider = ({ children }) => {
 
     const data = await response.json();
 
-    setUsers(data);
-    setLoading(false);
+    dispatch({
+      type: "GET_USERS",
+      data,
+    });
   };
 
   return (
-    <GithubContext.Provider value={{ users, loading, fetchUsers }}>
+    <GithubContext.Provider
+      value={{
+        users: state.users,
+        loading: state.loading,
+        fetchUsers,
+      }}
+    >
       {children}
     </GithubContext.Provider>
   );
