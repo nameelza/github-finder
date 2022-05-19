b3b73cd9885963c04d5ffc10ed4334a1eab0e95b
nameelza <82921161+nameelza@users.noreply.github.com>
2022-05-19 15:37:18 -0700

    Update AlertReducer.js

--- src/context/alert/AlertReducer.js
+++ src/context/alert/AlertReducer.js
@@ -1,5 +1,10 @@
 const alertReducer = (state, action) => {
-  
-}
+  switch (action.type) {
+    case "SET_ALERT":
+      return action.payload;
+    default:
+      return state;
+  }
+};
 
 export default alertReducer;
