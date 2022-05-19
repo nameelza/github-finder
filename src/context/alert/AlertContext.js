7570a222e16718fb374a7d110dfa9f71ce46279d
nameelza <82921161+nameelza@users.noreply.github.com>
2022-05-19 15:28:23 -0700

    Update AlertContext.js

--- src/context/alert/AlertContext.js
+++ src/context/alert/AlertContext.js
@@ -1,3 +1,9 @@
 import { createContext, useReducer } from "react";
+import alertReducer from "./AlertReducer";
 
 const AlertContext = createContext();
+
+const AlertProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(alertReducer, null);
+  
+};
