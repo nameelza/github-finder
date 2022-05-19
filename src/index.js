904dbbbd05c3f554d1b389eb3d7684b642d2763f
nameelza <82921161+nameelza@users.noreply.github.com>
2022-05-19 15:42:02 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,15 +4,18 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 import { GithubProvider } from "./context/github/GithubContext";
+import { AlertProvider } from "./context/alert/AlertContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
     <GithubProvider>
-      <Router>
-        <App />
-      </Router>
+      <AlertProvider>
+        <Router>
+          <App />
+        </Router>
+      </AlertProvider>
     </GithubProvider>
   </React.StrictMode>
 );
