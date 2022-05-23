1cb83ce66c4943dfa1a2f21ce673fa81fa40e84c
nameelza <82921161+nameelza@users.noreply.github.com>
2022-05-23 14:47:19 -0700

    Update User.jsx

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,16 +1,35 @@
+import { FaCodepen, FaStore, FaUserFriends, FaUsers } from "react-icons/fa";
 import { useContext, useEffect } from "react";
+import { Link } from "react-router-dom";
 import { useParams } from "react-router-dom";
 import GithubContext from "../context/github/GithubContext";
 
 function User() {
-  const { getUser, user } = useContext(GithubContext);
+  const { getUser, user, loading } = useContext(GithubContext);
   const params = useParams();
   useEffect(() => {
     getUser(params.login);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  return <div>{user.login}</div>;
+  const {
+    name,
+    type,
+    avatar_url,
+    location,
+    bio,
+    blog,
+    twitter_username,
+    login,
+    html_url,
+    followers,
+    following,
+    public_repos,
+    public_gists,
+    hireable,
+  } = user;
+
+  return loading ? <h1>Loading...</h1> : <div>{user.login}</div>;
 }
 
 export default User;
