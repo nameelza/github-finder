5dbd40ceac9f6085274de4a181034ba4ad45c195
nameelza <82921161+nameelza@users.noreply.github.com>
2022-05-23 14:21:42 -0700

    Update User.jsx

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -5,14 +5,12 @@ import GithubContext from "../context/github/GithubContext";
 function User() {
   const { getUser, user } = useContext(GithubContext);
   const params = useParams();
-  console.log(user)
-
   useEffect(() => {
     getUser(params.login);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  return <div>{params.login}</div>;
+  return <div>{user.login}</div>;
 }
 
 export default User;
