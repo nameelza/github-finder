a528ae6ccac3abf50e4aa9a68516841f61cf7801
nameelza <82921161+nameelza@users.noreply.github.com>
2022-05-23 15:35:23 -0700

    Update User.jsx

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -54,13 +54,17 @@ function User() {
 
           <div className="col-span-2">
             <div className="mb-6">
-              <h1 className="text-3xl card-title">
+              <h1 className="text-3xl text-white card-title">
                 {name || login}
                 <div className="ml-2 mr-1 badge badge-success">{type}</div>
                 {hireable && (
                   <div className="mx-1 badge badge-info">Hireable</div>
                 )}
               </h1>
+              <p className="text-white">{bio}</p>
+              <div className="mt-4 card-actions">
+                  <a href={html_url} target="_blank" rel="noreferrer" className="btn btn-outline">Visit Github Profile</a>
+              </div>
             </div>
           </div>
         </div>
