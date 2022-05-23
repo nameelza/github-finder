4bbc4e31e2c621b65e1423257a2e7d440a698f4e
nameelza <82921161+nameelza@users.noreply.github.com>
2022-05-23 15:03:35 -0700

    Update User.jsx

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -34,7 +34,21 @@ function User() {
   ) : (
     <div className="w-full mx-auto lg:w-10/12">
       <div className="mb-4">
-        <Link to="/" className="btn btn-ghost">Back To Search</Link>
+        <Link to="/" className="btn btn-ghost">
+          Back To Search
+        </Link>
+      </div>
+      <div className="grid grid-cols-1 xl:grid-cols-3 lg:grid-cols-3 md:grid-cols-3 mb-8 md:gap-8">
+        <div className="custom-card-image mb-6 md:mb-0">
+          <div className="rounded-lg shadow-xl card-image-full">
+            <figure>
+              <img src={avatar_url} alt={name} />
+            </figure>
+            <div className="card-body justify-end">
+              <h2 className="card-title mb-0">{name}</h2>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
