8cebc1004200deb5c6bb0dfbba7a3fc6dd3a90c3
nameelza <82921161+nameelza@users.noreply.github.com>
2022-05-23 16:17:54 -0700

    Update User.jsx

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -135,6 +135,17 @@ function User() {
               {following}
             </div>
           </div>
+
+          <div className="stat">
+            <div className="stat-figure text-secondary">
+              <FaCodepen className="text-3xl md:text-5xl" />
+            </div>
+            <div className="stat-title pr-5">Public Repos</div>
+            <div className="stat-value pr-5 text-3xl text-white md:text-4xl">
+              {public_repos}
+            </div>
+          </div>
+
         </div>
       </div>
     </>
