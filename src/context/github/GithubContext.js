019b7403d97514a070ff606e2f9097788078c6ea
nameelza <82921161+nameelza@users.noreply.github.com>
2022-05-24 14:28:02 -0700

    Update GithubContext.js

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -59,6 +59,28 @@ export const GithubProvider = ({ children }) => {
     }
   };
 
+  // Get user repos
+  const getRepos = async (login) => {
+    setLoading();
+
+    const response = await fetch(`${GITHUB_URL}/users/${login}/repos`, {
+      headers: {
+        Authorization: `token ${GITHUB_TOKEN}`,
+      },
+    });
+
+    if (response.status === 404) {
+      window.location = "/notfound";
+    } else {
+      const data = await response.json();
+
+      dispatch({
+        type: "GET_REPOS",
+        payload: data,
+      });
+    }
+  };
+
   const clearUsers = () => {
     dispatch({
       type: "CLEAR_USERS",
