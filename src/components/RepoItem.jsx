e3dbb0d108208104f01b23054b7d4d0b4cc1ad70
nameelza <82921161+nameelza@users.noreply.github.com>
2022-05-25 14:16:41 -0700

    Update RepoItem.jsx

--- src/components/RepoItem.jsx
+++ src/components/RepoItem.jsx
@@ -14,11 +14,29 @@ function RepoItem({ repo }) {
   return (
     <div className="mb-2 rounded-md card bg-gray-800 hover:bg-gray-900">
       <div className="card-body">
-        <h3 className="mb-2 text-xl font-semibold">
+        <h3 className="mb-2 text-xl font-semibold text-white">
           <a href={html_url}>
-            <FaLink />
+            <FaLink className="inline mr-1" />
+            {name}
           </a>
         </h3>
+        <p className="mb-3">{description}</p>
+        <div className="mr-2 badge badge-info badge-lg">
+          <FaEye className="mr-2" />
+          {watchers_count}
+        </div>
+        <div className="mr-2 badge badge-success badge-lg">
+          <FaStar className="mr-2" />
+          {stargazers_count}
+        </div>
+        <div className="mr-2 badge badge-error badge-lg">
+          <FaInfo className="mr-2" />
+          {open_issues}
+        </div>
+        <div className="mr-2 badge badge-warning badge-lg">
+          <FaUtensils className="mr-2" />
+          {forks}
+        </div>
       </div>
     </div>
   );
