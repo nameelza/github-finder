00e00d4fa6271212f903fbcfae4ea4ec7a3ebe70
nameelza <82921161+nameelza@users.noreply.github.com>
2022-05-25 16:27:15 -0700

    Update GithubActions.js

--- src/context/github/GithubActions.js
+++ src/context/github/GithubActions.js
@@ -3,15 +3,17 @@ const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
 const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
 
 const github = axios.create({
-    baseURL: GITHUB_URL,
-    headers: {Authorization: `token ${GITHUB_TOKEN}`},
-})
+  baseURL: GITHUB_URL,
+  headers: { Authorization: `token ${GITHUB_TOKEN}` },
+});
 
 export const searchUsers = async (text) => {
   const params = new URLSearchParams({
     q: text,
   });
 
+  const response = await github.get(`/search/users?${params}`);
+  console.log(response);
 };
 
 export const getUser = async (login) => {
