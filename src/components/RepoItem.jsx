c5cce173b617cbcd53b22d39d9011f23b13b2223
nameelza <82921161+nameelza@users.noreply.github.com>
2022-05-25 13:55:57 -0700

    Update RepoItem.jsx

--- src/components/RepoItem.jsx
+++ src/components/RepoItem.jsx
@@ -2,6 +2,16 @@ import { FaEye, FaInfo, FaLink, FaStar, FaUtensils } from "react-icons/fa";
 import PropTypes from "prop-types";
 
 function RepoItem({ repo }) {
+  console.log(repo);
+  const {
+    name,
+    description,
+    html_url,
+    forks,
+    open_issues,
+    watchers_count,
+    stargazers_count,
+  } = repo;
   return <div>{repo.name}</div>;
 }
 
