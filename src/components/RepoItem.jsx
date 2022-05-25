57da34da10f0579e3673d0cc29b2e4130cb73e18
nameelza <82921161+nameelza@users.noreply.github.com>
2022-05-25 13:52:35 -0700

    Update RepoItem.jsx

--- src/components/RepoItem.jsx
+++ src/components/RepoItem.jsx
@@ -1,5 +1,12 @@
+import { FaEye, FaInfo, FaLink, FaStar, FaUtensils } from "react-icons/fa";
+import PropTypes from "prop-types";
+
 function RepoItem({ repo }) {
   return <div>{repo.name}</div>;
 }
+
+RepoItem.propTypes = {
+  repo: PropTypes.object.isRequired,
+};
 
 export default RepoItem;
