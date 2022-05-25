34e1dbeb6bf3d8f13a8cc2eeeb0b083d1d6922ef
nameelza <82921161+nameelza@users.noreply.github.com>
2022-05-25 15:51:05 -0700

    Update GithubContext.js

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -3,9 +3,6 @@ import githubReducer from "./GithubReducer";
 
 const GithubContext = createContext();
 
-const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
-const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
-
 export const GithubProvider = ({ children }) => {
   const [state, dispatch] = useReducer(githubReducer, {
     users: [],
@@ -15,77 +12,10 @@ export const GithubProvider = ({ children }) => {
     loading: false,
   });
 
-  // Get single user
-  const getUser = async (login) => {
-    setLoading();
-
-    const response = await fetch(`${GITHUB_URL}/users/${login}`, {
-      headers: {
-        Authorization: `token ${GITHUB_TOKEN}`,
-      },
-    });
-
-    if (response.status === 404) {
-      window.location = "/notfound";
-    } else {
-      const data = await response.json();
-
-      dispatch({
-        type: "GET_USER",
-        payload: data,
-      });
-    }
-  };
-
-  // Get user repos
-  const getRepos = async (login) => {
-    setLoading();
-
-    const params = new URLSearchParams({
-      sort: "created",
-      per_page: 10,
-    });
-
-    const response = await fetch(
-      `${GITHUB_URL}/users/${login}/repos?${params}`,
-      {
-        headers: {
-          Authorization: `token ${GITHUB_TOKEN}`,
-        },
-      }
-    );
-
-    if (response.status === 404) {
-      window.location = "/notfound";
-    } else {
-      const data = await response.json();
-
-      dispatch({
-        type: "GET_REPOS",
-        payload: data,
-      });
-    }
-  };
-
-  const clearUsers = () => {
-    dispatch({
-      type: "CLEAR_USERS",
-    });
-  };
-
-  const setLoading = () => {
-    dispatch({
-      type: "SET_LOADING",
-    });
-  };
-
   return (
     <GithubContext.Provider
       value={{
         ...state,
-        clearUsers,
-        getUser,
-        getRepos,
         dispatch,
       }}
     >
