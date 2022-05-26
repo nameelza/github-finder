cac8811c237c29e51844962ddf1f28c6f8269e77
nameelza <82921161+nameelza@users.noreply.github.com>
2022-05-25 17:08:14 -0700

    Update GithubActions.js

--- src/context/github/GithubActions.js
+++ src/context/github/GithubActions.js
@@ -16,37 +16,46 @@ export const searchUsers = async (text) => {
   return response.data.items;
 };
 
-export const getUser = async (login) => {
-  const response = await fetch(`${GITHUB_URL}/users/${login}`, {
-    headers: {
-      Authorization: `token ${GITHUB_TOKEN}`,
-    },
-  });
+export const getUserAndRepos = async (login) => {
+  const [user, repos] = Promise.all(
+    github.get(`/users/${login}`),
+    github.get(`/users/${login}/repos`)
+  );
 
-  if (response.status === 404) {
-    window.location = "/notfound";
-  } else {
-    const data = await response.json();
-    return data;
-  }
+  return { user: user.data, repos: repos.data };
 };
 
-export const getRepos = async (login) => {
-  const params = new URLSearchParams({
-    sort: "created",
-    per_page: 10,
-  });
+// export const getUser = async (login) => {
+//   const response = await fetch(`${GITHUB_URL}/users/${login}`, {
+//     headers: {
+//       Authorization: `token ${GITHUB_TOKEN}`,
+//     },
+//   });
 
-  const response = await fetch(`${GITHUB_URL}/users/${login}/repos?${params}`, {
-    headers: {
-      Authorization: `token ${GITHUB_TOKEN}`,
-    },
-  });
+//   if (response.status === 404) {
+//     window.location = "/notfound";
+//   } else {
+//     const data = await response.json();
+//     return data;
+//   }
+// };
 
-  if (response.status === 404) {
-    window.location = "/notfound";
-  } else {
-    const data = await response.json();
-    return data;
-  }
-};
+// export const getRepos = async (login) => {
+//   const params = new URLSearchParams({
+//     sort: "created",
+//     per_page: 10,
+//   });
+
+//   const response = await fetch(`${GITHUB_URL}/users/${login}/repos?${params}`, {
+//     headers: {
+//       Authorization: `token ${GITHUB_TOKEN}`,
+//     },
+//   });
+
+//   if (response.status === 404) {
+//     window.location = "/notfound";
+//   } else {
+//     const data = await response.json();
+//     return data;
+//   }
+// };
