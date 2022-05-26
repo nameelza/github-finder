f3ed556a3e8b1ef47eaddff4be920fddd4532e54
nameelza <82921161+nameelza@users.noreply.github.com>
2022-05-25 17:15:12 -0700

    Update GithubReducer.js

--- src/context/github/GithubReducer.js
+++ src/context/github/GithubReducer.js
@@ -7,16 +7,11 @@ const githubReducer = (state, action) => {
         noUsers: action.payload.length === 0,
         loading: false,
       };
-    case "GET_USER":
+    case "GET_USER_AND_REPOS":
       return {
         ...state,
-        user: action.payload,
-        loading: false,
-      };
-    case "GET_REPOS":
-      return {
-        ...state,
-        repos: action.payload,
+        user: action.payload.user,
+        repos: action.payload.repos,
         loading: false,
       };
     case "SET_LOADING":
